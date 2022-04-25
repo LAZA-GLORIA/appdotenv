cd0f1378a07f9538c76a9a8c96f89ae5680b76e9
LAZA-GLORIA <gloria.besamba@gmail.com>
2022-04-25 11:43:17 +0200

    first app dotenv

---
+++ server.js
@@ -0,0 +1,3 @@
+require('dotenv').config() 
+console.log(`The server will run on port ${process.env.SERVER_PORT}`);
+console.log(`I am ${process.env.MY_NAME}, wilder in ${process.env.MY_CITY}, and I love ${process.env.MY_LANGUAGE}`);
